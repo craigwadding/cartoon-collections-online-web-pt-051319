ed4c4d63f14f3c2868e20fc9227366c042db4e46
craig wadding <craig.wadding@gmail.com>
2019-05-19 17:16:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)
   array.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
-  end
+
 end  
 roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
   
