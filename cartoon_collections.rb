18ac50ad68dbe11442a5c330f99417bde3acc1b4
craig wadding <craig.wadding@gmail.com>
2019-05-19 17:12:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
-def roll_call_dwarves(dwarves)
-  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
-end 
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
+end  
   
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |planeteer| planeteer_capitalize + "!" }
