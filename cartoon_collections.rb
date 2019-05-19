759ffc11b74e8c9ac246f814de74b14481acaf8a
craig wadding <craig.wadding@gmail.com>
2019-05-19 17:16:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
     puts "#{index + 1}. #{dwarf}"
 
 end  
-roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+
   
 def summon_captain_planet(array)
   array.collect { |i| i.capitalize << "!" }
